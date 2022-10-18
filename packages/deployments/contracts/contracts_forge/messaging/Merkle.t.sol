07cb02a864450454f9e403fa1ca3912edf966012
Rahul Sethuram <rahul@sethuram.com>
2022-10-18 10:40:06 +0400

    Add more debug tests

--- packages/deployments/contracts/contracts_forge/messaging/Merkle.t.sol
+++ packages/deployments/contracts/contracts_forge/messaging/Merkle.t.sol
@@ -23,4 +23,21 @@ contract MerkleTest is ForgeHelper {
       assertEq(_count - 1, i);
     }
   }
+
+  function test_Merkle__insert_realDataSpoke() public {
+    bytes32 _root;
+    uint256 _count;
+    (_root, _count) = merkle.insert(0xc1cd1b9ba9310c64a39b4e9b9cf5228b5357633c9c28ab7fdbec4d4b7c74d959);
+    (_root, _count) = merkle.insert(0x6ec95b703fabad4b653563971197ab8025dc649b4152ce68d8a81dc2fed1e48f);
+    emit log_named_bytes32("root", _root);
+    emit log_named_uint("count", _count);
+  }
+
+  function test_Merkle__insert_realDataAggregate() public {
+    bytes32 _root;
+    uint256 _count;
+    (_root, _count) = merkle.insert(0x003e20361347289ed4d3d4b132dea38fd618cf2b10a6d45f5163494f4dc9f77e);
+    emit log_named_bytes32("root", _root);
+    emit log_named_uint("count", _count);
+  }
 }
